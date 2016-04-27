a58a94bd9cb551de4e7103887fe52b63a7adcdd1
Phuoc <phuocpeter19@gmail.com>
2016-04-27 15:52:35 -0400

    Initial Commit

---
+++ Calculator Ultilities/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator Ultilities
+//
+//  Created by Tran Thai Phuoc on 2016-04-27.
+//  Copyright © 2016 Tran Thai Phuoc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
